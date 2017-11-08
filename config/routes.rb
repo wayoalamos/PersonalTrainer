5c4049933782efda782bc84b25bd3ce8851fd2a8
wayoalamos <malamos2@uc.cl>
2017-11-07 23:12:59 -0300

    new method implemented to crerate new users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "users" => "users#index"
   get "new" => "users#new"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post "users" => "users#create"
+  
 end
